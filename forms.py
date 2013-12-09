8591c30e75b12c1fe9fcfd6288c3d18b7a037587
Jerome Leclanche <jerome@leclan.ch>
2013-12-09 12:35:16 +0000

    Give summaries a max length of 80

--- forms.py
+++ forms.py
@@ -2,12 +2,12 @@ from wtforms import Form, BooleanField, TextField, TextAreaField, validators
 
 
 class DeleteForm(Form):
-	summary = TextField("Reason")
+	summary = TextField("Reason", [validators.length(max=80)])
 	watch_this = BooleanField("Watch this page")
 
 
 class EditForm(Form):
 	text = TextAreaField("Text")
-	summary = TextField("Summary")
+	summary = TextField("Summary", validators=[validators.length(max=80)])
 	minor_edit = BooleanField("This is a minor edit")
 	watch_this = BooleanField("Watch this page")
